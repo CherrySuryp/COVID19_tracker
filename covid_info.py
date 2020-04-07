21a1a4795ddb5ecbeedf5a82d64c12c963471c7d
Inhal3 <hackyourrself@gmail.com>
2020-04-07 17:33:21 +0300

    V 0.2

--- covid_info.py
+++ covid_info.py
@@ -7,27 +7,29 @@ translator = Translator()
 
 
 def worldInfo():
-    text = "<b>Статистика по миру:</b>\n\n" \
-           f"<b><u>Всего случаев заболевания</u></b>: {str(covid.get_total_confirmed_cases())}\n\n" \
-           f"<b><u>Всего активных зараженных</u></b>: {str(covid.get_total_active_cases())}\n\n" \
-           f"<b><u>Всего Смертей</u></b>: {str(covid.get_total_deaths())}\n\n" \
-           f"<b><u>Всего выздоровевших</u></b>: {str(covid.get_total_recovered())}"
+    text = f"<u>Статистика по миру</u>:\n\n" \
+           f"<b>Всего случаев заболевания</b>: {str(covid.get_total_confirmed_cases())}\n\n" \
+           f"<b>Всего активных зараженных</b>: {str(covid.get_total_active_cases())}\n\n" \
+           f"<b>Всего Смертей</b>: {str(covid.get_total_deaths())}\n\n" \
+           f"<b>Всего выздоровевших</b>: {str(covid.get_total_recovered())}"
     return text
 
 
 def countryInfo(country):
     try:
         status = covid.get_status_by_country_name(country)
-        text = f"<u>Страна</u>: <b>{translator.translate(status['country'], dest='ru').text}</b>\n\n" \
-               f"<b><u>Всего случаев заболевания</u></b>: {status['confirmed']}\n\n" \
-               f"<b><u>Всего активных зараженных</u></b>: {status['active']}\n\n" \
-               f"<b><u>Всего Смертей</u></b>: {status['deaths']}\n\n" \
-               f"<b><u>Всего выздоровевших</u></b>: {status['recovered']}\n" \
-               f"-------------------------------------------------\n" \
-               f'Кстати!\n' \
-               f'Узнать чем можно заняться во время самоизоляции можно в ' \
-               f'<a href="https://t.me/ClassesForAllOccasions"> нашем телеграм канале</a>'
+        text = f"<u>Статистика по стране:</u> <b>{translator.translate(status['country'], dest='ru').text}</b>\n\n" \
+               f"<b>Всего случаев заболевания</b>: {status['confirmed']}\n\n" \
+               f"<b>Всего активных зараженных</b>: {status['active']}\n\n" \
+               f"<b>Всего Смертей</b>: {status['deaths']}\n\n" \
+               f"<b>Всего выздоровевших</b>: {status['recovered']}\n"
         return text
     except ValueError:
         return "<b>Оопс.....</b>\n" \
                "Кажется Я не знаю такой страны"
+
+
+# '<b>Подробную информацию можно получить</b> '
+# '<a href = "https://coronavirus-spravka.ru/">на этом сайте</a>\n\n'
+# "<b>Или по телефону горячей линии по коронавирусу в РФ</b>\n"
+# "8(800)2000-112\n"
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import covid_info
 from googletrans import Translator
 import telebot
@@ -6,23 +7,30 @@ from telebot import types
 translator = Translator()
 
 bot = telebot.TeleBot(open('BOT_ACCESS').read())
-# Кнопки основного меню
-menu_keyboard = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True)
+
+# Кнопки начало
+menu_keyboard = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True, one_time_keyboard=True)
 stat_button = types.KeyboardButton(text='Статистика')
-info_button = types.KeyboardButton(text='Рекомендации')
-menu_keyboard.add(stat_button, info_button)
+menu_keyboard.add(stat_button)
 
 # Кнопки меню статистики
 stat_menu = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
-info_latest = types.KeyboardButton(text='Информация по странам')
-info_back = types.KeyboardButton(text='Назад в меню')
-info_ru = types.KeyboardButton(text='Информация по России')
-stat_menu.add(info_latest, info_ru, info_back)
+go_to_suggestions = types.KeyboardButton(text='Рекомендации')
+stat_menu.add(go_to_suggestions)
 
-# Кнопки меню рекомендаций
-suggestion_menu = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-suggestion_menu.add(info_back)
-message_id = int()
+# Кнопки меню информации и рекомендаций
+
+how_to_protect = types.KeyboardButton(text='Меры предосторожности')
+first_sug = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
+first_sug.add(how_to_protect)
+
+symptoms = types.KeyboardButton(text='Симптомы и группы риска')
+second_sug = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
+second_sug.add(symptoms)
+
+myth = types.KeyboardButton(text='Мифы')
+third_sug = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
+third_sug.add(myth)
 
 
 @bot.message_handler(commands=['start'])
@@ -31,7 +39,10 @@ def greet(message):
     message_id = message.chat.id
     bot.send_message(message.chat.id,
                      'Привет, <b>{0.first_name}</b>!\n'
-                     'Я бот, который поможет тебе узнать больше про Коронавирус(COVID19)'.format(message.from_user),
+                     'Я бот, который поможет вам узнать больше про Коронавирус(COVID19)\n\n'
+                     'Нажмите кнопку <b>"статистика"</b> '
+                     'и вы узнаете актуальную информацию о количестве зараженных'.format(
+                         message.from_user),
                      parse_mode='html', reply_markup=menu_keyboard)
 
 
@@ -40,33 +51,116 @@ def statMenu(message):
     # Меню статистики
     if message.text == 'Статистика':
         bot.send_message(message.chat.id, covid_info.worldInfo().format(message.from_user),
-                         parse_mode='html',
-                         reply_markup=stat_menu)
-
-    elif message.text == 'Информация по странам':
-        msg = bot.send_message(message.chat.id, 'Введите интересующую вас страну')
-        bot.register_next_step_handler(msg, stats)
-
-    elif message.text == 'Информация по России':
+                         parse_mode='html')
         bot.send_message(message.chat.id,
                          covid_info.countryInfo("Russia").format(message.from_user),
-                         parse_mode='html', reply_markup=menu_keyboard)
-
-    elif message.text == 'Назад в меню':
-        bot.send_message(message.chat.id, 'Вы вернулись в главное меню', reply_markup=menu_keyboard)
-    # Конец меню статистики ---------------------------------------------------------------------------
+                         parse_mode='html')
+        time.sleep(2)
+        bot.send_message(message.chat.id,
+                         'Пугает статистика?\n'
+                         'Меня вот пугает, хоть Я и бот\n'
+                         '--------------------------------------------------------------\n'
+                         'Получите полезные советы по профилактике коронавируса, нажав на кнопку <b>"Рекомендации"</b>',
+                         parse_mode='html', reply_markup=stat_menu)
 
     # Меню рекомендаций
     elif message.text == 'Рекомендации':
         bot.send_message(message.chat.id,
-                         "В этом меню вы можете узнать всю подробную информацию о коронавирусной инфекции\n"
-                         "--------------------------------------------------------------\n\n"
-                         '<b>Подробную информацию можно получить</b> '
-                         '<a href = "https://coronavirus-spravka.ru/">на этом сайте</a>\n\n'
-                         "<b>Или по телефону горячей линии по коронавирусу в РФ</b>\n"
-                         "8(800)2000-112\n",
+                         "Отлично!\n"
+                         "Я очень рад, что вам не безразлична безопасность вас и окружающих\n\n",
+                         parse_mode='html')
+        time.sleep(1.5)
+        bot.send_message(message.chat.id,
+                         "Хотите узнать про <b>меры предосторожности?</b>\n"
+                         "Тогда тыкайте на кнопку снизу",
                          parse_mode='html',
-                         disable_web_page_preview=True, reply_markup=suggestion_menu)
+                         reply_markup=first_sug)
+    elif message.text == 'Меры предосторожности':
+        # первый совет
+        bot.send_message(message.chat.id,
+                         'Итак, начнем ликбез')
+        time.sleep(1)
+        bot.send_message(message.chat.id,
+                         '<b>В первую очередь!</b>\n\n'
+                         'Регулярно мойте руки антибактериальным мылом\n'
+                         '<b>Или</b> обрабатывайте их спиртосодержащим антисептиком\n\n'
+                         '--------------------------------------------------------------\n'
+                         '"Но бот, все же и так моют руки"\n'
+                         'Скажете вы\n'
+                         '--------------------------------------------------------------\n\n'
+                         'Да, это так, но хочу обратить ваше внимание на:\n\n'
+                         '1)Мыло должно быть <b>антибактериальным</b>\n\n'
+                         '2)Антисептик должен содержать спирт концентрации <b>не менее 60%</b> '
+                         'Водка <b>не подойдет</b>',
+                         parse_mode='html')
+        #        time.sleep(11.5)
+        # второй совет
+        bot.send_message(message.chat.id,
+                         "<b>Во вторых</b>\n\n"
+                         "Соблюдайте правила респираторной гигиены\n\n"
+                         "При кашле и чихании прикрывайте рот и нос салфеткой, сгибом локтя, "
+                         "а лучше всего приобретите маску\n"
+                         "Cразу выкидывайте салфетку в контейнер для мусора "
+                         "и обрабатывайте руки спиртосодержащим антисептиком или мойте их водой с мылом\n\n"
+                         "<b>Зачем это нужно?</b>\n\n"
+                         "Все просто\n"
+                         "Прикрывание рта и носа при кашле и чихании салфеткой позволяет предотвратить распространение "
+                         "вирусов и других болезнетворных микроорганизмов\n\n"
+                         "<b>ВАЖНО!</b>\n\n"
+                         "Если при кашле или чихании прикрывать нос и рот ладонью, микробы могут попасть на ваши руки, "
+                         "а затем на предметы или на людей, к которым вы прикасаетесь.",
+                         parse_mode='html')
+        #        time.sleep(15)
+        bot.send_message(message.chat.id,
+                         "Фух, ну вроде все основное рассказал\n\n")
+        time.sleep(1.5)
+        bot.send_message(message.chat.id,
+                         "Далее у нас по плану <b>симптомы COVID-19</b>\n"
+                         "Просто нажми на кнопку", parse_mode='html', reply_markup=second_sug)
+    elif message.text == 'Симптомы и группы риска':
+        # симптомы
+        bot.send_message(message.chat.id,
+                         "<b>Группы риска</b>\n\n"
+                         "В группу риска входят:\n"
+                         "Беременные\n"
+                         "Люди с хроническими заболеваниями(астма и т.д.)\n"
+                         "Диабетики\n"
+                         "Младенцы\n"
+                         "Пожилые люди\n\n"
+                         "<b>Симптомы</b>\n\n"
+                         "Инкубационные период коронавируса составляет <b>до 14 дней</b>\n\n"
+                         "К наиболее распространенным симптомам COVID‑19 относятся:\n\n"
+                         "1.Повышение температуры тела\n"
+                         "2.Утомляемость\n"
+                         "3.Сухой кашель\n\n"
+                         "У некоторых людей могут отмечаться различные боли,"
+                         " заложенность носа, насморк, фарингит или диарея",
+                         parse_mode='html')
+        time.sleep(2)
+        bot.send_message(message.chat.id,
+                         "Если вдруг у вас есть хоть один из этих симптомов, то не стоит медлить с визитом к врачу!",
+                         reply_markup=third_sug)
+        time.sleep(2)
+    elif message.text == 'Мифы':
+        bot.send_message(message.chat.id,
+                         "<b>Немного информации про мифы</b>\n\n"
+                         "Вот несколько ложных представлений и мифов:\n\n"
+                         "1. Коронавирусная болезнь является неизлечимым заболеванием.\n\n"
+                         "2. Употребление алкоголя защищает от COVID‑19 и может быть опасным\n\n"
+                         "3. Вирус погибает при высокой температуре, поэтому нужно принять ванну\n\n"
+                         "4. Обработка поверхности всего тела этанолом или хлорной "
+                         "известью позволяет уничтожить новый коронавирус\n\n"
+                         "5. Вирусом можно заразиться сьев импортный банан"
+                         "6. Чеснок защищает от заражения")
+        bot.send_message(message.chat.id,
+                         "Вы сами то в это верите? Мне вот что то не очень верится\n"
+                         "Все вышеперечисленные высказывания являются <b>мифами</b> и никак научно не доказаны",
+                         parse_mode='html')
+
+
+#    elif message.text == 'Информация по странам':
+#        msg = bot.send_message(message.chat.id, 'Введите интересующую вас страну')
+#        bot.register_next_step_handler(msg, stats)
 
 
 def stats(message):
